1378ae99e8598cb2217f821b67975f5498daad4f
swati <swati.londhe9545@gmail.com>
2023-08-13 00:07:23 +0530

    second commit

---
+++ TestProject1/src/test2.java
@@ -0,0 +1,17 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		int a=10,b=20;
+		System.out.println("before swapping");
+		System.out.println("a= "+a+"  " +"b= "+b);
+		System.out.println("after swapping");
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a= "+a+"  " +"b= "+b);
+	}
+
+}
